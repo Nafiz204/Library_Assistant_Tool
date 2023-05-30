43057649ca5c4320ea7778f8781a1a582bd93cb1
Munem-1 <133049507+Munem-1@users.noreply.github.com>
2023-05-30 11:34:12 +0600

    Uploaded

---
+++ Library Managment System/src/Library_Service/Delete_Librarian.java
@@ -0,0 +1,202 @@
+
+package library_service;
+
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.io.FileWriter;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+
+
+public class Delete_Librarian extends javax.swing.JFrame {
+
+    
+    
+    public Delete_Librarian() {
+        initComponents();
+    }
+
+    
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lib_id = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        jLabel1.setText("DELETE LIBRARIAN");
+        jLabel1.setPreferredSize(new java.awt.Dimension(123, 22));
+
+        jLabel2.setText("ENTER THE ID No.:");
+        jLabel2.setPreferredSize(new java.awt.Dimension(100, 20));
+
+        lib_id.setPreferredSize(new java.awt.Dimension(75, 33));
+
+        jButton1.setText("DELETE");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jButton2.setText("Back");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(36, 36, 36)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(63, 63, 63)
+                                .addComponent(lib_id, javax.swing.GroupLayout.PREFERRED_SIZE, 115, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(114, 114, 114)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 159, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(0, 49, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(135, 135, 135)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(100, 100, 100)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lib_id, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(83, 83, 83)
+                .addComponent(jButton1)
+                .addContainerGap(91, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton2)
+                .addContainerGap())
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+
+        try{
+            int k=1;
+            String id = lib_id.getText();
+            File infile = new File("librarians.txt");
+            JDialog.setDefaultLookAndFeelDecorated(true);
+            if(infile.length() == 0){
+                JDialog.setDefaultLookAndFeelDecorated(true);
+                JOptionPane.showMessageDialog(Delete_Librarian.this,"There are no librarians. Please add librarians before deleting them!","Error!",JOptionPane.ERROR_MESSAGE);
+                new Head_Login_Success().setVisible(true);
+                this.setVisible(false);
+            }
+            else{
+                JDialog.setDefaultLookAndFeelDecorated(true);
+                int response = JOptionPane.showConfirmDialog(Delete_Librarian.this, "Are you sure you want to delete the librarian?", "Confirm?", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+                if (response == JOptionPane.YES_OPTION) {
+                    File tempfile = new File(infile.getAbsolutePath() + ".tmp");
+                    BufferedReader reader = new BufferedReader(new FileReader(infile));
+                    PrintWriter pw = new PrintWriter(new FileWriter(tempfile));
+                    String[] libs = new String[7];
+                    String read = "";
+                    while((read = reader.readLine())!=null){
+                        libs = read.split(";");
+                        if(!libs[0].trim().equals(id)){
+                            pw.println(read);
+                            pw.flush();
+                        }
+                        else
+                            k=0;
+                    }
+                    pw.close();
+                    reader.close();
+                    int x=0,y=0;
+                    if(k==0){
+                        if(!infile.delete()){
+                            JDialog.setDefaultLookAndFeelDecorated(true);
+                            JOptionPane.showMessageDialog(Delete_Librarian.this, "Sorry, an Unexpected error occurerd","Error 1!", JOptionPane.ERROR_MESSAGE);
+                            x=1;
+                        }
+                        if(!tempfile.renameTo(infile)){
+                            JDialog.setDefaultLookAndFeelDecorated(true);
+                            JOptionPane.showMessageDialog(Delete_Librarian.this, "Sorry, an Unexpected error occurerd","Error 2!", JOptionPane.ERROR_MESSAGE);
+                            y=1;
+                        }
+                        if(x==0 && y==0){
+                            File f = new File("Librarians" + id + ".txt");
+                            f.delete();
+                            JDialog.setDefaultLookAndFeelDecorated(true);
+                            JOptionPane.showMessageDialog(Delete_Librarian.this,"Librarian deleted successfully!");    
+                            new Head_Login_Success().setVisible(true);
+                            this.setVisible(false);
+                        }
+                    }
+                    else {
+                        JDialog.setDefaultLookAndFeelDecorated(true);
+                        JOptionPane.showMessageDialog(Delete_Librarian.this,"ID doesn't exist","ID Error!", JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+                else if(response==JOptionPane.NO_OPTION){
+                    JDialog.setDefaultLookAndFeelDecorated(true);
+                    JOptionPane.showMessageDialog(Delete_Librarian.this,"Delete Operation cancelled!");    
+                    new Head_Login_Success().setVisible(true);
+                    this.setVisible(false);
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+        new Head_Login_Success().setVisible(true);
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    
+    
+    public static void main(String args[]) {
+        
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Delete_Librarian().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField lib_id;
+    // End of variables declaration//GEN-END:variables
+}
