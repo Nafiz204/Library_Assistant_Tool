0adf20f31f56380a666f0c0674b883d6f73f1560
Nafiz Imtiaz <130889994+Nafiz204@users.noreply.github.com>
2023-06-05 10:54:38 +0600

    Updated Library_service.java
    This is the main class for this project.

---
+++ Library Managment System/src/Library_Service/Library_service.java
@@ -0,0 +1,19 @@
+
+package library_service;
+
+
+
+import java.io.FileWriter;
+import java.io.FileReader;
+
+public class Library_service {
+
+    
+
+    
+    public static void main(String[] args) {
+        new main_page().setVisible(true);
+        //this.setVisible(false);
+    }
+    
+}
